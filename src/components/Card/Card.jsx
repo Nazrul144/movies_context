019fabdc5563418f050b1f59934bd3dc41b39210
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-04-09 01:44:20 +0600

    cover photo added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Card = ({movie}) => {
-    const {movie_name, actor_actress, director, views, rating, category, budget} = movie;
+    const {movie_name, actor_actress, director, views, rating, category, budget, img} = movie;
     return (
         <div>
             <div className="card h-full bg-base-100 shadow-xl">
-                <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                <figure><img src={img} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">Movie Name: {movie_name}</h2>
                     <p> {actor_actress}</p>
